0451059169c2bf17e2cd21c6f2e0c8d87b560dd7
Dionte Murphy-Ayers <dtm9547@g.rit.edu>
2021-10-25 12:52:33 -0400

    assginment 2 complete

--- study.py
+++ study.py
@@ -6,7 +6,23 @@ def open_file(filename):
     except:
         print("Something went wrong...")
 
+def odd_lines(filename):
+    with open(filename) as a_file:
+        lineCounter = 1
+        for line in a_file:
+            if lineCounter % 2 != 0:
+                print(line,end="")
+                lineCounter = lineCounter + 1
+            else:
+                lineCounter = lineCounter + 1
+
+
 def main():
+    #AS1
     open_file(".\study_testFile.txt")
+
+    #AS2
+    odd_lines(".\study_testFile.txt")
+
 if __name__ == "__main__":
     main()
