958ad73abdc2a9d949931891222c9443d7c3ee52
Dionte Murphy-Ayers <dtm9547@g.rit.edu>
2021-10-25 12:32:47 -0400

    assginment 1 complete

---
+++ study.py
@@ -0,0 +1,13 @@
+def open_file(filename):
+    try:
+        with open(filename) as a_file:
+            print(len(a_file))
+            print("File opening working!")
+        
+    except:
+        print("Something went wrong...")
+
+def main():
+    open_file("study_testFile.txt")
+if __name__ == "__main__":
+    main()
