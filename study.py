983109f7adf7f8a9143555019bd69bffb9e21d49
Dionte Murphy-Ayers <dtm9547@g.rit.edu>
2021-10-25 13:49:02 -0400

    assginment 4 done ish

--- study.py
+++ study.py
@@ -16,9 +16,29 @@ def oddLines_totalLines(filename):
             else:
                 lineCounter = lineCounter + 1
     return lineCounter
-    
 
+def even_letters(filename):
+    final_str = " "
 
+    with open(filename) as a_file:
+        iterationStop = 30
+        letterCounter = 0
+        every = 20
+        for line in a_file:
+            lines = line.split(" ")
+            for word in lines:
+                words = word.split()
+                for letter in words:
+                    if iterationStop <= 0:
+                        break
+                    else:
+                        if letterCounter % 2 == 0:
+                            print(letter,end="")
+                            letterCounter = letterCounter + 1
+                        else:
+                            letterCounter = letterCounter + 1
+
+    return final_str
 
 def main():
     #AS1
@@ -28,6 +48,11 @@ def main():
     oddLines_totalLines(".\study_testFile.txt")
     aliceTotalLines = oddLines_totalLines(".\data\\alice.txt")
     print("Number of lines in 'alice.txt' is",aliceTotalLines)
+    
+    #AS4
+    even_letters(".\data\\alice.txt")
+
+
 
 if __name__ == "__main__":
     main()
